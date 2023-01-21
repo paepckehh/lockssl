d4445c6bf482f19247998e68f4edfbbe37dfa632
Paepcke, Michael <git@paepcke.de>
2023-01-21 18:33:30 +0000

    fix autocorrect glitch

--- include/openssl/rebuild.header.sh
+++ include/openssl/rebuild.header.sh
@@ -1,4 +1,4 @@
 sed -i '' -e "s/21 Jun 2022/LockSSL ( RELEASE:$LOCKSSL_RELEASE ) ( COMMIT:$(cat openssl/.commit | cut -c 1-12) )/g" opensslv.h
 sed -i '' -e "s/xx XXX xxxx/LockSSL ( RELEASE:$LOCKSSL_RELEASE ) ( COMMIT:$(cat openssl/.commit | cut -c 1-12) )/g" opensslv.h
-sed -i '' -e 's/"1.1.1"/"111"/g' opensslv.h
-sed -i '' -e 's/"1.1"/"111"/g' opensslv.h
+sed -i '' -e "s/\"1.1.1\"/\"111\"/g" opensslv.h
+sed -i '' -e "s/\"1.1\"/\"111\"/g" opensslv.h
